8e4f2e41ab033548adf69fdb99697a72b0cc8a74
BrayanCordeiro <linsbvl73@gmail.com>
2024-01-18 18:33:59 -0300

    Testando Create, Update e DeletePerson com mockito

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/brayancordeiro/restwithspringboot/mockito/services/PersonServicesTest.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/brayancordeiro/restwithspringboot/mockito/services/PersonServicesTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.brayancordeiro.restwithspringboot.data.vo.v1.PersonVO;
 import com.brayancordeiro.restwithspringboot.model.Person;
 import com.brayancordeiro.restwithspringboot.repositories.PersonRepository;
 import com.brayancordeiro.restwithspringboot.services.PersonServices;
@@ -53,24 +54,72 @@ MockPerson input;
 		assertEquals("Last Name Test1", result.getLastName());
 		assertEquals("Female", result.getGender());
 	}
-	
-	@Test
-	void testFindAll() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testCreatePerson() {
-		fail("Not yet implemented");
+		Person entity = input.mockEntity(1); 
+		entity.setId(1L);
+		
+		Person persisted = entity;
+		persisted.setId(1L);
+		
+		PersonVO vo = input.mockVO(1);
+		vo.setKey(1L);
+		
+		when(repository.save(entity)).thenReturn(persisted);
+		
+		var result = service.createPerson(vo);
+		
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("Addres Test1", result.getAdress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
 	}
 
 	@Test
-	void testUpdatePerson() {
-		fail("Not yet implemented");
+	void testUpdatePeson() {
+		Person entity = input.mockEntity(1); 
+		
+		Person persisted = entity;
+		persisted.setId(1L);
+		
+		PersonVO vo = input.mockVO(1);
+		vo.setKey(1L);
+		
+
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+		
+		var result = service.updatePerson(vo);
+		
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("Addres Test1", result.getAdress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
 	}
 
 	@Test
 	void testDeletePerson() {
+		Person entity = input.mockEntity(1); 
+		entity.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		
+		service.deletePerson(1L);
+	}
+	
+	@Test
+	void testFindAll() {
 		fail("Not yet implemented");
 	}
 
