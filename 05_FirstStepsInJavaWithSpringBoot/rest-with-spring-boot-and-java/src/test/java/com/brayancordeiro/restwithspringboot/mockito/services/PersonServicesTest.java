aaf425bdcf7394079d62a8fb85ebba65a9e69357
BrayanCordeiro <linsbvl73@gmail.com>
2024-01-18 18:00:17 -0300

    Testando findById e geração de links HATEOAS com mockito

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/brayancordeiro/restwithspringboot/mockito/services/PersonServicesTest.java
@@ -0,0 +1,77 @@
+package com.brayancordeiro.restwithspringboot.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.brayancordeiro.restwithspringboot.model.Person;
+import com.brayancordeiro.restwithspringboot.repositories.PersonRepository;
+import com.brayancordeiro.restwithspringboot.services.PersonServices;
+import com.brayancordeiro.restwithspringboot.unittests.mapper.mocks.MockPerson;
+
+class PersonServicesTest {
+
+MockPerson input;
+	
+	@InjectMocks
+	private PersonServices service;
+	
+	@Mock
+	PersonRepository repository;
+	
+	@BeforeEach
+	void setUpMocks() throws Exception {
+		input = new MockPerson();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testFindById() {
+		Person entity = input.mockEntity(1); 
+		entity.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		
+		var result = service.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("Addres Test1", result.getAdress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+	
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreatePerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdatePerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeletePerson() {
+		fail("Not yet implemented");
+	}
+
+}
