1e9f3ed1a8a2a0a2224bb7060f44346f5076df9a
BrayanCordeiro <linsbvl73@gmail.com>
2024-01-22 18:51:31 -0300

    Criação da entidade Book e seus respectivos repository e migrations

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/brayancordeiro/restwithspringboot/repositories/BookRepository.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/brayancordeiro/restwithspringboot/repositories/BookRepository.java
@@ -3,7 +3,7 @@ package com.brayancordeiro.restwithspringboot.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.brayancordeiro.restwithspringboot.model.Person;
+import com.brayancordeiro.restwithspringboot.model.Book;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>{}
+public interface BookRepository extends JpaRepository<Book, Long>{}
